875c394278a59c3d024368e3cb2f1727a03c59ea
Patrick de la Guardia <delaguardia.co@gmial.com>
2018-09-10 14:17:02 -0400

    completed tutorial

---
+++ src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+<h1>About, hey {{ their_name }} </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+  data() {
+     return {
+       their_name: this.$route.params.name
+     }
+    },
+    }
+
+</script>
